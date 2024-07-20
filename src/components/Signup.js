d5687792ab797e147fd7394b8283d9242e7c5895
aman52kwah <amankwaamichael52@gmail.com>
2024-07-20 00:00:34 -0700

    first commit

---
+++ src/components/Signup.js
@@ -0,0 +1,45 @@
+import {useRef} from 'react'
+import {Form ,Card} from 'react-bootstrap'
+
+
+export default function Signup() {
+    const emailRef= useRef()
+    const passwordRef= useRef()
+    const passwordConfirmRef= useRef()
+    
+    
+  return (
+    <>
+      <Card>
+<Card.body>
+    <h2 className='text-center mb-4'>Sign Up</h2>
+    <Form>
+        <Form.Group id='email'>
+            <Form.Label>Email</Form.Label>
+            <Form.Control type='email' ref={emailRef} required/>
+        </Form.Group>
+        <Form.Group id='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control type='password' ref= {passwordRef} required/>   
+        </Form.Group> 
+
+        <Form.Group id='password-confirm'>
+            <Form.Label>Password Confirmation</Form.Label>
+            <Form.Control type='password' ref= {passwordConfirmRef} required/>    
+        </Form.Group>
+
+<button type='submit' className='w-100'>Sign Up</button>
+
+
+    </Form>
+</Card.body>
+
+
+
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        Already have an account? Log In
+      </div>
+    </>
+  )
+}
